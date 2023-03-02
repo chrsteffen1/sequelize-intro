9c3a059e0fdf5c376a004fd1b220b73fe4e6daee
Chrsteffen <Chrsteffen1@gmail.com>
2023-03-02 18:11:48 -0500

    add a seeders file

---
+++ seeders/20230302230852-cat.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const date = new Date()
+
+    await queryInterface.bulkInsert('Cats', [{
+      name: 'Snuffles',
+      breed: 'Tabby',
+      age: 5,
+      createdat: date,
+      updatedAT: date
+    }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Cats', null, {})
+  }
+};
